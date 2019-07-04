b2378d5ed19dca005865a146c44ac619cb31bb3e
Kang.Yuchan <ikimaku0517@gmail.com>
2019-07-04 12:58:54 +0900

    delete mutation

--- DB/db.js
+++ DB/db.js
@@ -24,14 +24,14 @@ let movies = [
 export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
 };
 
 export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length){
-        movie = cleanedMovies;
+        movies = cleanedMovies;
         return true;
     } else {
         return false
@@ -40,7 +40,7 @@ export const deleteMovie = (id) => {
 
 export const addMovie = (name, score) => {
     const newMovie = {
-        id: `${movies.length}`,
+        id: `${movies.length + 1}`,
         name,
         score
     };
